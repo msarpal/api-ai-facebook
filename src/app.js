254f08a56cb5ec1f4997358cd01e568b60bdea9f
msarpal <mohitsarpal50@gmail.com>
2017-05-12 15:29:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ const JSONbig = require('json-bigint');
 const async = require('async');
 
 const REST_PORT = (process.env.PORT || 5000);
-const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
+const APIAI_ACCESS_TOKEN = '294f8c2c15a84a8bb2e586ddb69b60fd';
 const APIAI_LANG = process.env.APIAI_LANG || 'en';
-const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
-const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
+const FB_VERIFY_TOKEN = 'abcd1234';
+const FB_PAGE_ACCESS_TOKEN = 'EAASOIcZAHi8sBALi8OzyM9pIUTYys3QdQj2c31clh2XEyvWGtwdCXWD88bKUY6jil9YR8xBuHkL00PMrCy8Rb3QrdeXOWjTecLkeucH8blQBiaqspoUSsS0iZBUtEqZAyoFsOESKi9GZADeXyQM8b2kJJVGyimJJQCNyUuB2zgZDZD';
 const FB_TEXT_LIMIT = 640;
 
 const FACEBOOK_LOCATION = "FACEBOOK_LOCATION";
